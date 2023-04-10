ac64488994708972a6f426220eedf791fcf2f79d
AhmetHakanC <megatipter@hotmail.com>
2023-04-10 03:13:16 +0300

    Giriş kısmı türkçe oldu. ödev sayfa tasarımı değişti

--- resources/views/web/auth/student_login.blade.php
+++ resources/views/web/auth/student_login.blade.php
@@ -13,7 +13,7 @@
     <div class="max-w-md w-full space-y-8">
         <div>
             <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow">
-            <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
+            <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Hesabınıza Giriş Yapın</h2>
             {{--            <p class="mt-2 text-center text-sm text-gray-600">--}}
             {{--                Or--}}
             {{--                <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> start your 14-day free trial </a>--}}
@@ -38,11 +38,11 @@
             <div class="flex items-center justify-between">
                 <div class="flex items-center">
                     <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded">
-                    <label for="remember-me" class="ml-2 block text-sm text-gray-900"> Remember me </label>
+                    <label for="remember-me" class="ml-2 block text-sm text-gray-900"> Beni Hatırla </label>
                 </div>
 
                 <div class="text-sm">
-                    <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> Forgot your password? </a>
+                    <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> Şifremi Unuttum? </a>
                 </div>
             </div>
 
@@ -54,7 +54,7 @@
               <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
             </svg>
           </span>
-                    Sign in
+                    Giriş Yap
                 </button>
             </div>
         </form>
--- resources/views/web/homework/homeworks.blade.php
+++ resources/views/web/homework/homeworks.blade.php
@@ -11,50 +11,96 @@
         </div>
 
         <div class="py-2 w-full lg:px-8 flex justify-center">
-            <div class="mt-8 relative">
-                <div class="relative w-full pb-6 -mb-6 overflow-x-auto">
-                    <ul role="list" class="inline-flex space-x-5 sm:mx-6 lg:mx-0 lg:space-x-0 lg:grid lg:grid-cols-4 lg:gap-x-8">
+            <ul role="list" class="inline-flex space-x-5 sm:mx-6 lg:mx-0 lg:space-x-0 lg:grid lg:grid-cols-5 lg:gap-x-8 p-10 w-full">
 
-                        @foreach($homeworks as $homework)
-                            <li class="w-64 inline-flex flex-col text-center lg:w-64 mb-20 mr-10">
-                                <div class="group relative bg-gray-100 rounded p-3 h-500">
-                                    <div class="w-full bg-gray-200 rounded-md overflow-hidden aspect-w-1 aspect-h-1">
-                                        @php
-                                            $imageUrl= "https://img.freepik.com/free-vector/school-supplies-books-pencils-apple_24908-56504.jpg?w=900&t=st=1680994850~exp=1680995450~hmac=47dde76ff2ad57b31097bc8ea678f7457ea761546735250efa6f53fec3d3fc04";
-                                            if($homework->lesson=="Kimya") {
-                                                $imageUrl="https://imgyukle.com/f/2023/04/09/QeRPRP.png";
-                                            } elseif($homework->lesson=="Matematik") {
-                                                $imageUrl="https://www.resimupload.org/images/2023/04/09/download.png";
-                                            } elseif($homework->lesson=="Fizik") {
-                                                $imageUrl="https://www.resimupload.org/images/2023/04/09/7677494.jpg";
-                                            } elseif($homework->lesson=="Biyoloji") {
-                                                $imageUrl="https://www.resimupload.org/images/2023/04/09/biyoloji-ders-notlari.jpg";
-                                            }
-                                        @endphp
-                                        <img src="{{ $imageUrl }}" alt="resim" class="w-full h-full object-center object-cover group-hover:opacity-75">
+                @foreach($homeworks as $homework)
+                    <a href="#" class="w-200 h-300 bg-gray-100 rounded-xl">
+                        <div class="w-full h-full p-4">
+
+                            <div class="w-150 h-150">
+                                @php
+                                    $imageUrl= "https://img.freepik.com/free-vector/school-supplies-books-pencils-apple_24908-56504.jpg?w=900&t=st=1680994850~exp=1680995450~hmac=47dde76ff2ad57b31097bc8ea678f7457ea761546735250efa6f53fec3d3fc04";
+                                    if($homework->lesson=="Kimya"){
+                                        $imageUrl="https://imgyukle.com/f/2023/04/09/QeRPRP.png";
+                                    } elseif($homework->lesson=="Matematik") {
+                                        $imageUrl="https://www.resimupload.org/images/2023/04/09/download.png";
+                                    } elseif($homework->lesson=="Fizik") {
+                                        $imageUrl="https://www.resimupload.org/images/2023/04/09/7677494.jpg";
+                                    } elseif($homework->lesson=="Biyoloji"){
+                                    $imageUrl="https://img.freepik.com/free-vector/school-supplies-books-pencils-apple_24908-56504.jpg?w=900&t=st=1680994850~exp=1680995450~hmac=47dde76ff2ad57b31097bc8ea678f7457ea761546735250efa6f53fec3d3fc04";
+                                    }
+                                    @endphp
+                                <img src="{{ $imageUrl }}" alt="resim" class="w-full h-full object-center object-cover">
+
+                                <p class="text-center text-gray-500 font-medium text-s">{{ $homework->lesson }}</p>
+                                <hr class="border-t-1 border-gray-300 my-2">
+                                <p class="text-center text-gray-600 font-medium text-m">{{ $homework->subject }}</p>
+                                <hr class="border-t-1 border-gray-300 my-2">
+                            </div>
+{{--                            Bu divin tabana yapışması lazım ama beceremedim--}}
+                            <div id="deneme" class="flex flex-col">
+                                <div class="flex-col w-full">
+                                    <div class="flex p-2 rounded justify-between align-middle">
+                                        <p style="font-size: 14px" class="text-center text-gray-600 font-medium">Oluşturulma tarihi:  </p>
+                                        <p style="font-size: 14px" class="text-center text-gray-600 font-medium">{{date('d-m-Y', strtotime($homework->created_at))}}</p>
                                     </div>
-                                    <div class="mt-2">
-                                        <p class="text-sm text-gray-500">{{$homework->lesson}}</p>
-                                        <p class="text-sm text-gray-500">{{$homework->subject}}</p>
-                                        <p class="text-gray-900 text-xs mt-3">Oluşturulma Tarihi: {{date('d-m-Y', strtotime($homework->created_at))}}</p>
-                                        <p class="mt-1 text-gray-900 text-xs">Son Teslim Tarihi: {{date('d-m-Y', strtotime($homework->deadline))}}</p>
-                                        @if($homework->isDone == 1)
-                                            <p class="text-green-800 font-bold"> Ödev tamamlandı </p>
-                                        @else
-                                            <p class="text-red-800 font-bold"> Ödev tamamlanmadı </p>
-                                        @endif
+                                    <div class="flex bg-amber-300 p-2 rounded justify-between align-middle">
+                                        <p style="font-size: 14px" class="text-center text-gray-600 font-medium">Son Teslim Tarihi:  </p>
+                                        <p style="font-size: 14px" class="text-center text-gray-600 font-medium">{{date('d-m-Y', strtotime($homework->deadline))}}</p>
                                     </div>
                                 </div>
-                                <h4 class="sr-only">Available colors</h4>
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
-            </div>
+                                <div class="flex-row">
+                                    @if($homework -> isDone == 1)
+                                    <p>Ödev Durumu: Tamamlandı</p>
+                                        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-n8Y2yxE+EuLdntoGb5/Q/6Z2Q2qwX5AVFgN6uN5OYfv7BojKo6CmR+xgGm50ddj0cTxXHNOU+QIXmNCezFEfYw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+                                    @else
+                                    <p>Ödev Durumu: Tamamlanmadı</p>
+                                    @endif
+                                    <button class="w-full border-2 border-green-500 hover:bg-green-500 mt-3 rounded p-1 text-gray-600 hover:text-white font-bold text-l">Ödevi Gönder</button>
+                                </div>
+                            </div>
 
-            <div class="mt-12 flex px-4 sm:hidden">
-                <a href="#" class="text-sm font-semibold text-indigo-600 hover:text-indigo-500">See everything<span aria-hidden="true"> &rarr;</span></a>
-            </div>
+                        </div>
+                    </a>
+
+
+{{--                           <li class="w-64 inline-flex flex-col text-center lg:w-64 mb-20 mr-10">--}}
+{{--                               <div class="bg-gray-400 w-200 h-300">--}}
+{{--                                   <p class="h-200">Selamlar</p>--}}
+{{--                               </div>--}}
+
+
+{{--                               <div class="group relative bg-gray-100 rounded p-3 h-500">--}}
+{{--                                   <div class="w-full bg-gray-200 rounded-md overflow-hidden aspect-w-1 aspect-h-1">--}}
+{{--                                       @php--}}
+{{--                                           $imageUrl= "https://img.freepik.com/free-vector/school-supplies-books-pencils-apple_24908-56504.jpg?w=900&t=st=1680994850~exp=1680995450~hmac=47dde76ff2ad57b31097bc8ea678f7457ea761546735250efa6f53fec3d3fc04";--}}
+{{--                                           if($homework->lesson=="Kimya") {--}}
+{{--                                               $imageUrl="https://imgyukle.com/f/2023/04/09/QeRPRP.png";--}}
+{{--                                           } elseif($homework->lesson=="Matematik") {--}}
+{{--                                               $imageUrl="https://www.resimupload.org/images/2023/04/09/download.png";--}}
+{{--                                           } elseif($homework->lesson=="Fizik") {--}}
+{{--                                               $imageUrl="https://www.resimupload.org/images/2023/04/09/7677494.jpg";--}}
+{{--                                           } elseif($homework->lesson=="Biyoloji") {--}}
+{{--                                               $imageUrl="https://www.resimupload.org/images/2023/04/09/biyoloji-ders-notlari.jpg";--}}
+{{--                                           }--}}
+{{--                                       @endphp--}}
+{{--                                       <img src="{{ $imageUrl }}" alt="resim" class="w-full h-full object-center object-cover group-hover:opacity-75">--}}
+{{--                                   </div>--}}
+{{--                                   <div class="mt-2">--}}
+{{--                                       <p class="text-sm text-gray-500">{{$homework->lesson}}</p>--}}
+{{--                                       <p class="text-sm text-gray-500">{{$homework->subject}}</p>--}}
+{{--                                       <p class="text-gray-900 text-xs mt-3">Oluşturulma Tarihi: {{date('d-m-Y', strtotime($homework->created_at))}}</p>--}}
+{{--                                       <p class="mt-1 text-gray-900 text-xs">Son Teslim Tarihi: {{date('d-m-Y', strtotime($homework->deadline))}}</p>--}}
+{{--                                       @if($homework->isDone == 1)--}}
+{{--                                           <p class="text-green-800 font-bold"> Ödev tamamlandı </p>--}}
+{{--                                       @else--}}
+{{--                                           <p class="text-red-800 font-bold"> Ödev tamamlanmadı </p>--}}
+{{--                                       @endif--}}
+{{--                                   </div>--}}
+{{--                               </div>--}}
+{{--                           </li>--}}
+                @endforeach
+            </ul>
         </div>
     </div>
 
