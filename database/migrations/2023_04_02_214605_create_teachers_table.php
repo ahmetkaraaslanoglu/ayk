defaf5d62289cc216c61fe0dd23645d176802743
AhmetHakanC <megatipter@hotmail.com>
2023-04-12 02:00:53 +0300

    teacher table da değişiklik

--- database/migrations/2023_04_02_214605_create_teachers_table.php
+++ database/migrations/2023_04_02_214605_create_teachers_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('teacher', function (Blueprint $table) {
+        Schema::create('teachers', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(\App\Models\SchoolClass::class);
             $table->string('name');
